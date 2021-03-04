dce2d85c0b931aa6f158ff28d64d7896ca25485e
gitcoder-23 <prabhatece21@gmail.com>
2021-03-04 20:30:36 +0530

    Initial Go shop commit

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+
+
+// Banner image download from https://www.pexels.com/
+
+const Banner = () => {
+
+    return (
+        <div className="container">
+        <header>
+        <div className="headerText">
+            <div>
+            <h1>Wonderful shoping website</h1>
+            <p>Enjoy shopping with Go Shop</p>
+            </div>
+        </div>
+
+        </header>
+        </div>
+
+    )
+
+};
+
+export default Banner;
